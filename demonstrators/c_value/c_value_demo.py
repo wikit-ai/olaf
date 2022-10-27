04560ad033a11c15f899e349a2be6efab4e120d3
msesboue <matthias.sesboue@insa-rouen.fr>
2022-10-27 14:42:54 +0200

    update demo to use relative corpus path

--- demonstrators/c_value/c_value_demo.py
+++ demonstrators/c_value/c_value_demo.py
@@ -1,8 +1,14 @@
+import os
 from term_extraction.term_extraction_service import Term_Extraction
 from data_preprocessing.data_preprocessing_service import Data_Preprocessing
+from config.core import DATA_PATH, config
 
 
 def main() -> None:
+
+    demo_data_file_path = config['data_preprocessing']['corpus']['corpus_path']
+    config['data_preprocessing']['corpus']['corpus_path'] = os.path.join(
+        DATA_PATH, demo_data_file_path)
 
     data_prep = Data_Preprocessing()
     data_prep._set_corpus()
