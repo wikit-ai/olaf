a8cb937c07b66df3f733f709e598e758ab681430
Marion Schaeffer <marion@wikit.ai>
2022-12-05 08:32:39 +0100

    Update concept extraction

--- src/concept_extraction/concept_extraction_service.py
+++ src/concept_extraction/concept_extraction_service.py
@@ -2,14 +2,17 @@ from typing import Any, Dict, List
 
 from commons.ontology_learning_schema import CandidateTerm, KR
 from concept_extraction.concept_extraction_methods.group_by_synonyms import GroupBySynonyms
+from config.core import config
 
 class ConceptExtraction():
 
-    def __init__(self, candidate_terms: List[CandidateTerm], options: Dict[str, Any]) -> None:
+    def __init__(self, candidate_terms: List[CandidateTerm], config: Dict[str, Any] = config['concept_extraction']) -> None:
         self.candidate_terms = candidate_terms
         self.kr = KR()
-        self.options = options
+        self.config = config
 
-    def group_by_synonyms(self):
+    def group_by_synonyms(self) -> None:
+        """This method merges candidate terms in a concept if they have value or synonyms in common.
+        """
         group_by_syn = GroupBySynonyms(self.candidate_terms, self.kr)
         group_by_syn()
