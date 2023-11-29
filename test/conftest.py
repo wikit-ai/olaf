6c9a9a30eb99fda653d53cb2e6c7e54c134fef39
msesboue <matthias.sesboue@insa-rouen.fr>
2023-11-29 15:51:39 +0100

    add exclude ner in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -4,5 +4,5 @@ import spacy
 
 @pytest.fixture(scope="session")
 def en_sm_spacy_model():
-    spacy_model = spacy.load("en_core_web_sm")
+    spacy_model = spacy.load("en_core_web_sm", exclude=["ner"])
     return spacy_model
