4acf81a48a2f4b54cf9db9ef9ac941b87a0e2344
Schaeffer Marion <marion.schaeffer@insa-rouen.fr>
2023-12-29 17:29:22 +0000

    feat(semantic-similarity-enrichment)

--- test/conftest.py
+++ test/conftest.py
@@ -6,3 +6,9 @@ import spacy
 def en_sm_spacy_model():
     spacy_model = spacy.load("en_core_web_sm", exclude=["ner"])
     return spacy_model
+
+
+@pytest.fixture(scope="session")
+def en_md_spacy_model():
+    spacy_model = spacy.load("en_core_web_md", exclude=["ner"])
+    return spacy_model
