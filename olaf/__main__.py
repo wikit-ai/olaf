54c28db5631ebc2c216bcc70ae407e8f498ecd75
ba-talibe <omarba2015@gmail.com>
2024-05-15 14:56:09 +0200

    added option to execute all demonstrations

--- olaf/__main__.py
+++ olaf/__main__.py
@@ -20,6 +20,7 @@ def list_pipeline_names(module_name):
     module_dir = os.path.dirname(module.__file__)
 
     # List files in the module directory
+    pipelines = []
     for filename in os.listdir(module_dir):
         # Check if the file is a Python module and doesn't match the pattern
         if (
@@ -28,7 +29,18 @@ def list_pipeline_names(module_name):
             and filename[:-3] != "runner"
         ):
             module_name = filename[:-3]  # Remove the ".py" extension
-            print("\t", module_name)
+        pipelines.append(module_name)
+
+    pipelines = [
+        filename[:-3]
+        for filename in os.listdir(module_dir)
+        if (
+            filename.endswith(".py")
+            and not re.match(r"(__\w+__|_\w+)\.py", filename)
+            and filename[:-3] != "runner"
+        )
+    ]
+    return pipelines
 
 
 def run_pipeline(pipeline_name):
@@ -45,7 +57,8 @@ def run_pipeline(pipeline_name):
 
 def list_pipeline(args):
     print("Listing pipelines...")
-    list_pipeline_names("olaf.scripts")
+    for pipeline in list_pipeline_names("olaf.scripts"):
+        print("\t", pipeline)
 
 
 def show_pipeline(pipeline_name):
