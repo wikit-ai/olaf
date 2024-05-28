97dc029942f7dd38b61e8e78f467683a88c5b05b
ba-talibe <omarba2015@gmail.com>
2024-05-28 15:08:53 +0200

    use current working directory as result path

--- olaf/scripts/llm_pipeline.py
+++ olaf/scripts/llm_pipeline.py
@@ -160,14 +160,10 @@ class PipelineRunner(Runner):
         self.pipeline.run()
 
         kr_serialiser = KRJSONSerialiser()
-        kr_serialisation_path = os.path.join(
-            os.getenv("RESULTS_PATH"), "llm_pipeline", "llm_pipeline_kr.json"
-        )
+        kr_serialisation_path = os.path.join(os.getcwd(), "llm_pipeline_kr.json")
         kr_serialiser.serialise(kr=self.pipeline.kr, file_path=kr_serialisation_path)
 
-        kr_rdf_graph_path = os.path.join(
-            os.getenv("RESULTS_PATH"), "llm_pipeline", "llm_pipeline_kr_rdf_graph.ttl"
-        )
+        kr_rdf_graph_path = os.path.join(os.getcwd(), "llm_pipeline_kr_rdf_graph.ttl")
         self.pipeline.kr.rdf_graph.serialize(kr_rdf_graph_path, format="ttl")
 
         print(f"Nb concepts: {len(self.pipeline.kr.concepts)}")
